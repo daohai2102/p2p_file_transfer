b2f96cd916e0bfb69a11d37fb2640592bbff3e44
Phuc Nguyen <phucnguyenbn.9x@gmail.com>
2018-12-26 17:41:58 +0700

    connect to index server

--- src/peer/update_file_list.c
+++ src/peer/update_file_list.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 #include "update_file_list.h"
 #include "../common.h"
@@ -23,5 +24,35 @@ void announceDataPort(int sockfd){
 	if (n_bytes <= 0){
 		print_error("send data port to index server");
 		exit(1);
+	}
+}
+
+void update_file_list(char *dir_name){
+	/* connect to index server */
+	struct sockaddr_in servsin;
+	bzero(&servsin, sizeof(servsin));
+
+	servsin.sin_family = AF_INET;
+
+	char *servip;
+	char buf[22];
+	printf("Enter server's IP addr and port in the form of <IP>:<port>\n");
+	scanf("%s", buf);
+	getchar();	/* remove '\n' character from the stdin */
+
+	/* parse the buf to get ip address and port */
+	servip = strtok(buf, ":");
+	servsin.sin_addr.s_addr = inet_addr(servip);
+	servsin.sin_port = htons(atoi(strtok(NULL, ":")));
+
+	int servsock = socket(AF_INET, SOCK_STREAM, 0);
+	if (servsock < 0){
+		print_error("socket");
+		exit(2);
+	}
+
+	if (connect(servsock, (struct sockaddr*) &servsin, sizeof(servsin)) < 0){
+		print_error("connect");
+		exit(3);
 	}
 }
